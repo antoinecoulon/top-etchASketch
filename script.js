5fbe83b27af3224ebd549b377a36adc383c2ef8f
Antoine COULON <a.antoinecoulon@gmail.com>
2024-09-23 11:43:08 +0200

    Feature: mouse hover

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const container = document.getElementById("container");
 
 for(let i=1; i <= 256; i++){
     const divGrid = document.createElement("div");
+    divGrid.className = "divElement";
     container.appendChild(divGrid);
 }
 
-console.log("OK");
+const divElement = document.querySelectorAll(".divElement");
+divElement.forEach(div => {
+    div.addEventListener("mouseover", (event) => {
+        div.style.backgroundColor = "blue"; // Modifier le style directement
+    });
+
+    // Optionnel : Réinitialiser la couleur lors du "mouseout" (quand la souris quitte l'élément)
+    div.addEventListener("mouseout", (event) => {
+        div.style.backgroundColor = ""; // Remettre la couleur d'origine
+    });
+});
